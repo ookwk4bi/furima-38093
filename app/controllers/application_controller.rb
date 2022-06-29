de732f2372230c20040873b79cd853bcd325019f
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-06-29 18:00:01 +0900

    エラーの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :basic_auth
-    before_action :configure_permitted_parameters,if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
 
     private
@@ -9,10 +9,8 @@ class ApplicationController < ActionController::Base
       authenticate_or_request_with_http_basic do |username, password|
         username == 'ookwk4bi' && password == '8909'
       end
-      
-      def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-      end
     end
-    
-end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
+    end
+    end
