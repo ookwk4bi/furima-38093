3bea6afc3cafdaffa6242edefcf34139dc0b3a26
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-06-29 17:16:09 +0900

    userのバリデーション設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :nickname, presence: true
-         validates :profile, presence: true
-        validates :occupation, presence: true
-        validates :position, presence: true
+         validates :last_name, presence: true
+        validates :first_name, presence: true
+        validates :last_name_kana, presence: true
+        validates :first_name_kana, presence: true
+        validates :last_name, presence: true
+        validates :birthday, presence: true
+        
 end
