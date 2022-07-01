b2ac321d6dc6e28dc2cb26826a59d8ab8f3b2d36
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-07-01 14:07:03 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def create
+    @item = Item.new(item_params)
 end
+
+
+private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
