6c38824294a041a1242b41ccc9288aff6e5d7772
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-07-04 18:21:26 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはログインページに促す
-  before_action :authenticate_user!, except: [:index]
+  
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
