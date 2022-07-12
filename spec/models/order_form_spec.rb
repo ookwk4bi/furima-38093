5532fe61056075a8fb6d98edff4432ee3578ca4b
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-07-12 15:20:04 +0900

    修正４

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -103,6 +103,11 @@ RSpec.describe OrderForm, type: :model do
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include('Phone number is invalid')
       end
+      it '電話番号９桁未満あると保存できないこと' do
+        @order_form.phone_number = 12_345_678
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include('Phone number is invalid')
+      end
       it 'トークンが空だと保存できないこと' do
         @order_form.token = nil
         @order_form.valid?
